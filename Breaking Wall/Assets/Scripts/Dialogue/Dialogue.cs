818eeb872310adc0c2c35370b26416b19a4a217a
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-02-17 21:55:55 +0100

    Dialogues basic

---
+++ Breaking Wall/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dialogue : MonoBehaviour
+{
+    [TextArea(5, 10)]
+    public string[] texts;
+    DialogueManager myDM;
+
+    [Header("Dialogue after This")]
+    public Dialogue nextDialogue;
+
+    [Header("Typing Settings")]
+    [Range(0, 1)]
+    public float typeDelay = 0.05f;
+
+
+
+    [Space(5)]
+    [Header("Events")]
+    public UnityEvent atStartEvent;
+    public UnityEvent atEndEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDM = FindObjectOfType<DialogueManager>();
+
+        if (myDM == null)
+        {
+            Debug.LogWarning("No Dialogue Manager in scene");
+        }
+    }
+
+    public void trigger()
+    {
+        if (myDM != null)
+        {
+            myDM.startDialogue(this);
+        }
+    }
+
+
+
+}
---
+++ Breaking Wall/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Objects")]
+    public TextMeshProUGUI dialogueText;
+    public Image image;
+
+    static DialogueManager instance;
+    AudioSource source;
+
+    [Header("Typing Sound Settings")]
+    public AudioClip typingSound;
+
+    [Range(0, 5)]
+    public float pitch = 1f;
+    [Range(0, 1)]
+    public float volume = 0.5f;
+
+    bool busy;
+
+
+    void Awake()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+            source = GetComponent<AudioSource>();
+
+            if (dialogueText == null)
+            {
+                Debug.LogWarning("No dialogue Text in DialogueManager!");
+            }
+
+            if (image == null)
+            {
+                Debug.LogWarning("No image in DialogueManager!");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Deleting " + name + " because " + instance.name + " is singleton");
+            Destroy(this);
+
+        }
+    }
+
+    public void startDialogue(Dialogue dialogue)
+    {
+        if (!busy)
+        {
+            show();
+            busy = true;
+            StartCoroutine(TypeText(dialogue));
+
+        }
+
+    }
+
+    IEnumerator TypeText(Dialogue dialogue)
+    {
+        //Get every Sentence
+        foreach (string s in dialogue.texts)
+        {
+            char[] characters = s.ToCharArray();
+            dialogueText.text = "";
+
+            //Get every char from sentence
+            foreach (char c in characters)
+            {
+                dialogueText.text += c;
+
+                //Sound should be played now
+                playSound();
+
+                yield return new WaitForSeconds(dialogue.typeDelay);
+            }
+
+            //Wait for input to end sentence
+
+        }
+
+        if (dialogue.nextDialogue != null)
+        {
+            dialogue.atEndEvent.Invoke();
+            StartCoroutine(TypeText(dialogue.nextDialogue));
+        }
+        else
+        {
+            hide();
+            busy = false;
+        }
+
+
+    }
+
+    void hide()
+    {
+        image.enabled = false;
+        dialogueText.enabled = false;
+
+    }
+
+    void playSound() {
+
+        if (!source.isPlaying && typingSound != null)
+        {
+            source.clip = typingSound;
+            
+            source.pitch = pitch;
+            source.volume = volume;
+
+            source.Play();
+        }
+    }
+
+    void show()
+    {
+        image.enabled = true;
+        dialogueText.enabled = true;
+
+    }
+}
