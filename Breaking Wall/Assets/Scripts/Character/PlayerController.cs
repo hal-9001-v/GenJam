5f4d9b8c0684803bfe5c7ca8e521e99cbc6a1714
dexaxi <dexaxi12@gmail.com>
2021-02-18 00:20:10 +0100

    Ewe

--- Breaking Wall/Assets/Scripts/Character/PlayerController.cs
+++ Breaking Wall/Assets/Scripts/Character/PlayerController.cs
@@ -6,39 +6,34 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    public PlayerControls myPlayerControls;
+    //GOs
+    PlayerControls myPlayerControls;
+    private Rigidbody myRb;
+     
+    //MovementVals
     private Vector2 moveInput;
+    
+    //ControlVars
     private bool jump;
-    private bool dive;
-
+    public bool isGrounded;
+    public int currentState;
+    //State
+    public enum State
+    {
+        GROUNDED = 0,
+        JUMPING = 1,
+        DIVING = 2,
+    }
+    
     private void Awake() {
         
         myPlayerControls = new PlayerControls();
-       
+        myRb = GetComponent<Rigidbody>();
+        
     }
 
-        private void OnEnable()
-    {
-        myPlayerControls.Enable();
-    }
     
-    private void OnDisable()
-    {
-        myPlayerControls.Disable();
-    }
-
-    public void setPlayerControls(PlayerControls pc){
-
-        pc.DefaultActionMap.Movement.performed += ctx => ctx.ReadValue<Vector2>();
-
-        pc.DefaultActionMap.Movement.canceled += ctx => ctx.ReadValue<Vector2>();
-
-        pc.DefaultActionMap.Jump.performed += ctx => jump = true;
-
-        pc.DefaultActionMap.Dive.performed += ctx => dive = true;
     
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -48,6 +43,75 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckGrounded(isGrounded);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        Debug.Log(other.name);
+        if (other.tag == "Ground")
+        {
+            isGrounded = true;
+            Debug.Log("Grounded");
+        }
         
     }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.tag == "Ground")
+        {
+            isGrounded = false;
+            Debug.Log("Not Grounded");
+        }
+     
+    }
+
+    private void CheckGrounded(bool grounded) {
+
+        if (grounded) { currentState = (int)State.GROUNDED; } else currentState = (int)State.JUMPING;
+
+
+    }
+
+    private void Jump(){
+
+        Debug.Log("Jumped!");
+        if(currentState == (int) State.GROUNDED) myRb.velocity = new Vector3(0, 14, 0);
+        currentState = (int)State.JUMPING;
+        isGrounded = false;
+
+    }
+
+    private void Dive(){
+
+        Debug.Log("Dove!");
+
+    }
+
+    private void OnEnable()
+    {
+        myPlayerControls.Enable();
+    }
+    
+    private void OnDisable()
+    {
+        myPlayerControls.Disable();
+    }
+
+    
+   public void setPlayerControls(PlayerControls pc){
+
+        pc.DefaultActionMap.Movement.performed += ctx => ctx.ReadValue<Vector2>();
+
+        pc.DefaultActionMap.Movement.canceled += ctx => ctx.ReadValue<Vector2>();
+        
+        pc.DefaultActionMap.Jump.performed += ctx => Jump();
+
+        pc.DefaultActionMap.Dive.performed += ctx => Dive();
+        
+
+    }
+
 }
