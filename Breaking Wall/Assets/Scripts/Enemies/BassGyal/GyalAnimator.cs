16fe541290efa29d6451dc7d95c7cacc3593bc78
dexaxi <dexaxi12@gmail.com>
2021-02-22 18:10:22 +0100

    Vicente: push prefabs of Bosses

---
+++ Breaking Wall/Assets/Scripts/Enemies/BassGyal/GyalAnimator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyalAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    private BassGyalScript myWall;
+
+    private void Awake()
+    {
+        if (myWall == null) myWall = FindObjectOfType<BassGyalScript>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myWall.moveInput != Vector2.zero)
+        {
+
+            anim.SetBool("Running", true);
+
+        }
+        else anim.SetBool("Running", false);
+
+        if (myWall.isGrounded)
+        {
+
+            anim.SetBool("Grounded", true);
+
+        }
+        else anim.SetBool("Grounded", false);
+
+        if (myWall.hitting)
+        {
+
+            anim.SetBool("Hitting", true);
+
+        }
+        else anim.SetBool("Hitting", false);
+
+
+        if (myWall.shooting)
+        {
+
+            anim.SetBool("Shoot", true);
+
+        }
+        else anim.SetBool("Shoot", false);
+
+
+
+        if (myWall.isHit)
+        {
+
+            anim.SetBool("Hit", true);
+
+        }
+        else anim.SetBool("Hit", false);
+    }
+}
---
+++ Breaking Wall/Assets/Scripts/Enemies/SevillanaBoss/AndaluzAnimator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndaluzAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    private WallDaLuciaScript myWall;
+
+    private void Awake()
+    {
+        if (myWall == null) myWall = FindObjectOfType<WallDaLuciaScript>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        if (myWall.isHit)
+        {
+
+            anim.SetBool("Hit", true);
+
+        }
+        else anim.SetBool("Hit", false);
+
+        if (myWall.sleeping)
+        {
+
+            anim.SetBool("Sleep", true);
+
+        }
+        else anim.SetBool("Sleep", false);
+    }
+}
